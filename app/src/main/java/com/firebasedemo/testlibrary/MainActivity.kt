04ea420a2b47162f01a96abad94f1acd1899b624
Jai Mishra <jai.mishra@rsvrtech.com>
2020-04-16 10:29:41 +0530

    upadte

--- app/src/main/java/com/firebasedemo/testlibrary/MainActivity.kt
+++ app/src/main/java/com/firebasedemo/testlibrary/MainActivity.kt
@@ -1,14 +1,7 @@
 package com.firebasedemo.testlibrary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.firebasedemo.testlibrary.response.ApiResponse
-import com.jetbrains.handson.mpp.mobile.createApplicationScreenMessage
-import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
